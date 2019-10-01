3f2664af1471c9f2938a49b1be63745215d1556a
Tanner York <tannerwyork@gmail.com>
2019-10-01 11:15:49 -0700

    Add virus class tests

--- virus.py
+++ virus.py
@@ -7,9 +7,8 @@ class Virus(object):
         self.mortality_rate = mortality_rate
 
 
+#### Test Virus Class ####
 def test_virus_instantiation():
-    #TODO: Create your own test that models the virus you are working with
-    '''Check to make sure that the virus instantiator is working.'''
     virus = Virus("HIV", 0.8, 0.3)
     assert virus.name == "HIV"
     assert virus.repro_rate == 0.8
